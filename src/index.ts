5e5838ae06d4d9b6f03c10a06c7c8cb19e770e61
Cameron Yick <cameron.yick@gmail.com>
2020-11-21 19:36:53 -0500

    add some dots to the game field

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { makeSprite, t, GameProps } from "@replay/core";
 import { WebInputs, RenderCanvasOptions } from "@replay/web";
 import { iOSInputs } from "@replay/swift";
 
-import { Turtle } from './turtle';
-import { posix } from "path";
+import { Turtle } from "./turtle";
+// import { posix } from "path";
+import { Engine, World, Bodies, Composite } from "matter-js";
 
 // defined in webpack
 declare const ASSET_NAMES: {};
@@ -20,6 +21,7 @@ export const options: RenderCanvasOptions = {
 };
 
 const TURTLE_SPEED = 5;
+const TURTLE_RADIUS = 15;
 
 export const gameProps: GameProps = {
   id: "Game",
@@ -48,8 +50,39 @@ type GameState = {
   targetY: number;
 };
 
+const engine = Engine.create();
+
 export const Game = makeSprite<GameProps, GameState, WebInputs | iOSInputs>({
-  init() {
+  init({ props, device }) {
+    // const circleA = Bodies.circle(Math.floor(device.random() * device.size.width / 2),
+    //                               Math.floor(device.random() * device.size.height / 2), 6 );
+    //  const circleB =
+
+    const circleBodies = new Array(5).fill(0).map((_) => {
+      return Bodies.circle(
+        Math.floor((device.random() * device.size.width) / 2),
+        Math.floor((device.random() * device.size.height) / 2),
+        Math.floor((device.random() * 5 )) + 2,
+        {
+          render: {
+            fillStyle: device.random() > 0.5 ? 'red': 'blue'
+          }
+        }
+      );
+    });
+
+    const turtleBody = Bodies.circle(
+      0,0, TURTLE_RADIUS,
+      {
+        render: {
+          fillStyle: 'green'
+        }
+      }
+    )
+
+    World.add(engine.world, circleBodies);
+    World.add(engine.world, turtleBody);
+
     return {
       posX: 0,
       posY: 0,
@@ -59,7 +92,7 @@ export const Game = makeSprite<GameProps, GameState, WebInputs | iOSInputs>({
   },
 
   loop({ state, device }) {
-    const { pointer, keysDown } = device.inputs;
+    const { pointer, keysDown, keysJustPressed } = device.inputs;
     const { posX, posY } = state;
     let { targetX, targetY } = state;
 
@@ -70,48 +103,81 @@ export const Game = makeSprite<GameProps, GameState, WebInputs | iOSInputs>({
     }
 
     // Turtle Movement
-    if (keysDown['ArrowLeft']) {
+    if (keysDown["ArrowLeft"]) {
       targetX -= TURTLE_SPEED;
     }
-    if (keysDown['ArrowRight']) {
+    if (keysDown["ArrowRight"]) {
       targetX += TURTLE_SPEED;
     }
 
-    if (keysDown['ArrowUp']) {
+    if (keysDown["ArrowUp"]) {
       targetY += TURTLE_SPEED;
     }
-    if (keysDown['ArrowDown']) {
+    if (keysDown["ArrowDown"]) {
       targetY -= TURTLE_SPEED;
     }
 
+    // When difference is small enough, let's just say it matches.
+    let deltaX = (targetX - posX) / 10;
+    if (Math.abs(deltaX) < 0.01) {
+      deltaX = 0;
+      targetX = posX;
+    }
+
+    let deltaY = (targetY - posY) / 10;
+    if (Math.abs(deltaY) < 0.01) {
+      deltaY = 0;
+      targetY = posY;
+    }
+
+    // console.log(deltaX);
+
     return {
-      posX: posX + (targetX - posX) / 10,
-      posY: posY + (targetY - posY) / 10,
+      posX: posX + deltaX,
+      posY: posY + deltaY,
       targetX,
       targetY,
     };
   },
 
   render({ state }) {
+    let turtleTilt = 0;
+
+    if (state.targetX < state.posX) {
+      turtleTilt = -30 * (Math.abs(state.targetX - state.posX) / 30);
+    } else if (state.targetX > state.posX) {
+      turtleTilt = 30 * (Math.abs(state.targetX - state.posX) / 30);
+    }
+
+    if (state.targetY < state.posY) {
+      turtleTilt = -30 * (Math.abs(state.targetY - state.posY) / 30);
+    } else if (state.targetY > state.posY) {
+      turtleTilt = 30 * (Math.abs(state.targetY - state.posY) / 30);
+    }
+
+    const worldBodies = Composite.allBodies(engine.world);
+
     return [
       t.text({
         color: "turquoise",
-        text: "TurtleShield",
-        y: 50,
+        text: "Hurtle Hockey",
+        y: 0,
       }),
-      // t.image({
-      //   testId: "icon",
-      //   x: state.posX,
-      //   y: state.posY,
-      //   fileName: "icon.png",
-      //   width: 50,
-      //   height: 50,
-      // }),
       Turtle({
-        id: 'turtle',
+        id: "turtle",
         x: state.posX,
         y: state.posY,
-      })
+        rotation: turtleTilt,
+        turtleRadius: 15
+      }),
+      ...worldBodies.map((body) => {
+        return t.circle({
+          radius: body.circleRadius ?? 1,
+          color: body.render.fillStyle ?? 'black',
+          x: body.position.x,
+          y: body.position.y,
+        });
+      }),
     ];
   },
 });
--- src/turtle.ts
+++ src/turtle.ts
@@ -1,36 +1,79 @@
-import { makeSprite, t } from "@replay/core";
+// import { SpriteBaseProps } from '@replay/core';
+// import { SpriteBaseProps } from "@replay/core/props";
+import { makeSprite, t} from "@replay/core";
 
-export const turtleWidth = 25;
-export const turtleHeight = 25;
+import { WebInputs } from "@replay/web";
+import { iOSInputs } from "@replay/swift";
 
+
+// export const turtleWidth = 30;
+// export const turtleHeight = 30;
 
 // Color picker
 // https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=536DFE
 
+type SpriteBaseProps = any; // can't import
+
+type Props = {
+  turtleRadius: 15
+
+};
+type State = {
+  frame: number;
+  localFrame: number; // time to spend in 1 turtle state
+};
+
+
 // T for Texture
-export const Turtle = makeSprite({
-  render() {
+export const Turtle = makeSprite<Props, State, WebInputs | iOSInputs>({
+  init() {
+    return {
+      frame: 0,
+      localFrame: 0,
+    };
+  },
+
+  loop({ state, props }) {
+
+    // Prevent turtle animation from looping too fast.
+    const shouldAnimate = (props as SpriteBaseProps).rotation !== 0;
+    const shouldIncrementLocalFrame = shouldAnimate && state.localFrame < 10; // local
+
+    return {
+      frame: shouldIncrementLocalFrame || !shouldAnimate ? state.frame : state.frame + 1,
+      localFrame: shouldIncrementLocalFrame ? state.localFrame + 1 : 0,
+    };
+  },
+  render({ props, state }) {
+
+    const turtleWidth = props.turtleRadius * 2;
+
     return [
 
       t.circle({
         radius: turtleWidth,
-        color: "#0043ca",
-        // mask: mask.circle({
-        //   radius: turtleWidth - 5,
-        // })
+        color: "#4ebaaa",
       }),
       t.circle({
         radius: turtleWidth - 5,
         color: "white",
       }),
 
-
-
-      t.rectangle({
+      t.spriteSheet({
+        fileName: "turtle.png",
+        columns: 4,
+        rows: 1,
+        index: state.frame % 4,
         width: turtleWidth,
-        height: turtleHeight,
-        color: "#4ebaaa",
+        height: turtleWidth,
       }),
+
+
+      // t.rectangle({
+      //   width: turtleWidth,
+      //   height: turtleHeight,
+      //   color: "#4ebaaa",
+      // }),
     ];
   },
 });
